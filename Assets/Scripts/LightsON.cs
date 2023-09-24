3a546df1b2a5db0d6058e72d34fe39895b4bc928
Lajote <124814132+Lajote@users.noreply.github.com>
2023-09-24 11:58:35 -0500

    asdasdsa

---
+++ Assets/Scripts/LightsON.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightsON : MonoBehaviour
+{
+
+    public SpriteRenderer darkness;
+
+    private float targetAlpha;
+    public float alphaChangeSpeedON = 10.0f; // Adjust this value to control the speed of alpha transition to ON.
+    bool POWER = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetAlpha = 0f; // Initialize the target alpha to 0.
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lightsOn();
+        if (POWER)
+        {
+            darkness.color = new Color(darkness.color.r, darkness.color.g, darkness.color.b, Mathf.Lerp(darkness.color.a, targetAlpha, alphaChangeSpeedON * Time.deltaTime));
+        }
+    }
+
+    void lightsOn()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            POWER = true;
+            
+        }
+
+
+        
+    }
+
+}
